b1593fa5c63fc85b5746a0f9d8785b86de514b2a
mahmoud elsheshtawy <103583850+MahmoudElsheshtawy@users.noreply.github.com>
2023-08-27 02:23:53 -0700

    alart

--- src/App.js
+++ src/App.js
@@ -18,8 +18,13 @@ const handleAddtodo =()=>{
     title:newtitle,
     decription:newdiscription,
     id:shortid.generate()
-  };
 
+  
+  }
+  if (newtitle === '') {
+    return alert('Please enter the task')
+    
+  }
 
 // add and change in the state
   // save perv sate 
@@ -112,7 +117,7 @@ useEffect(()=>{
                         <div className="todo-input-item">
                   
                           <button type="button" className="button" onClick={handleAddtodo}>
-                            <span className="button__text">Add Task</span>
+                            <span className="button__text" >Add Task</span>
                             <span className="button__icon"><svg xmlns="http://www.w3.org/2000/svg" width="24" viewBox="0 0 24 24" strokeWidth="2" strokeLinejoin="round" strokeLinecap="round" stroke="currentColor" height="24" fill="none" className="svg"><line y2="19" y1="5" x2="12" x1="12"></line><line y2="12" y1="12" x2="19" x1="5"></line></svg></span>
                           </button>
 
